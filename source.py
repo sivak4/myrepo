90e03ca5c8c3ed4754b32e7f3eeecdc1a1bfeccf
sivak4 <32291734+sivak4@users.noreply.github.com>
2017-09-26 18:58:23 +0530

    Create source.py

---
+++ source.py
@@ -0,0 +1,14 @@
+ foo = [
+            'some string',
+         'another string',
+           'short string'
+]
+print foo
+['some string', 'another string', 'short string']
+
+ bar = 'this is ' \
+       'one long string ' \
+'that is split ' \
+     'across multiple lines'
+ print bar
+this is one long string that is split across multiple lines
